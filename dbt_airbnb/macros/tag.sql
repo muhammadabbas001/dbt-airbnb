43025e0a97f9c9c1dc8967d7da894b10b61d2fd5
Muhammad Abbas <muhammdabbas.cs@gmail.com>
2026-06-20 12:03:02 +0500

    bronze silver gold layer done - create meta data driven development for one big table in gold layer

---
+++ dbt_airbnb/macros/tag.sql
@@ -0,0 +1,9 @@
+{% macro tag(col) %}
+
+    case
+        when {{ col }} < 100 then 'low'
+        when {{ col }} < 200 then 'medium'
+        else 'high'
+    end
+
+{% endmacro %}
---
+++ dbt_airbnb/macros/timmer.sql
@@ -0,0 +1,5 @@
+{% macro trimmer(col_name, node) %}
+
+    {{ col_name | trim | upper }}
+
+{% endmacro%}
